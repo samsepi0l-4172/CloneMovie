ea61e1aec3a0e23cdd6e4b6d338270c3ed130e98
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2025-11-22 01:24:27 +0900

    8.2.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import Home from './routes/Home';
 import './App.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
 
 function App() {
-  return <Home />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route
+          path='/about'
+          element={<About />}
+        />
+        <Route
+          path='/'
+          exact={true}
+          element={<Home />}
+        />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
