49b391499f93507ec3b40b53e53e135cb73e32e9
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2025-11-21 20:39:25 +0900

    5.3.2

--- src/App.js
+++ src/App.js
@@ -25,6 +25,10 @@ class App extends React.Component {
     console.log('component rendered');
   }
 
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+
   render() {
     console.log("I'm rendering");
     return (
