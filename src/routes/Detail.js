19678922299f6bd7ba99c36a9844e6cc339a6d1d
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2025-11-22 02:12:36 +0900

    finish

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-function Detail(props) {
-  console.log(props);
-  return <span>hello</span>;
+function Detail() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!location?.state) {
+      navigate('/', { replace: true });
+    }
+  }, [location, navigate]);
+
+  if (!location?.state) {
+    return null;
+  }
+
+  return <span>{location.state.title}</span>;
 }
 
 export default Detail;
