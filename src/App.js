364f8a106f4ba37aac488339460f185af38d9c23
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2025-11-21 19:20:04 +0900

    3.4.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-function Potato() {
-  return <h3>I love potato</h3>;
+function Movie() {
+  return <h1>I love potato</h1>;
 }
 
 function App() {
   return (
     <div>
       <h1>Hello!!!!</h1>
-      <Potato />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
     </div>
   )
 }
