58b9fad408c2aa0e0ad0cef8cfbad7eafc55130c
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2025-11-22 00:48:58 +0900

    6.4.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -23,10 +24,14 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? 'Loading...'
-          : movies.map((movie) => {
+      <section class='container'>
+        {isLoading ? (
+          <div class='loader'>
+            <span class='loader__text'>Loading...</span>
+          </div>
+        ) : (
+          <div className='movies'>
+            {movies.map((movie) => {
               console.log(movie);
               return (
                 <Movie
@@ -39,7 +44,9 @@ class App extends React.Component {
                 />
               );
             })}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({ id, year, title, summary, poster }) {
-  return <h4>{title}</h4>;
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div className='movie'>
+      <img
+        src={poster}
+        alt={title}
+        title={title}
+      />
+      <h3 className='movie__title'>{title}</h3>
+      <h5 className='movie__year'>{year}</h5>
+      <p className='movie__summary'>{summary}</p>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-  id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
