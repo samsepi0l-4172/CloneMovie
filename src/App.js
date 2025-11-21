99fa38c778a9dfc827ef12847483aaf4e75b4b63
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2025-11-21 19:26:05 +0900

    3.4.1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
-function Movie() {
-  return <h1>I love potato</h1>;
+function Food(props) {
+  return <h1>I like {props.fav}</h1>;
 }
+
+// function Food(props) {
+//   { fav } = props;
+//   return <h1>I like {fav}</h1>;
+// }
+
+// function Food({ fav }) {
+//   return <h1>I like {fav}</h1>;
+// }
 
 function App() {
   return (
     <div>
       <h1>Hello!!!!</h1>
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
+      <Food fav="hamberger" />
     </div>
   )
 }
