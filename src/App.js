c22955389d47369c20263e2d9f78c79f04171322
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2025-11-21 20:32:22 +0900

    5.1.0

--- src/App.js
+++ src/App.js
@@ -1,102 +1,27 @@
-import PropTypes from 'prop-types';
+import React from 'react';
 
-// function Food(props) {
-//   return <h1>I like {props.fav}</h1>;
-// }
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-// function Food(props) {
-//   { fav } = props;
-//   return <h1>I like {fav}</h1>;
-// }
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
 
-function Food({ name, rating, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-const foodLike = [
-  {
-    id: 1,
-    name: 'hamberger',
-    image:
-      'https://cdn.pixabay.com/photo/2025/08/28/18/42/hamberger-10037363_1280.jpg',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'pizza',
-    image:
-      'https://cdn.pixabay.com/photo/2025/08/28/18/42/hamberger-10037363_1280.jpg',
-    rating: '4',
-  },
-  {
-    id: 3,
-    name: 'chicken',
-    image:
-      'https://cdn.pixabay.com/photo/2025/08/28/18/42/hamberger-10037363_1280.jpg',
-    rating: 3,
-  },
-  {
-    id: 4,
-    name: 'ramen',
-    image:
-      'https://cdn.pixabay.com/photo/2025/08/28/18/42/hamberger-10037363_1280.jpg',
-    rating: 2,
-  },
-  {
-    id: 5,
-    name: 'kimbab',
-    image:
-      'https://cdn.pixabay.com/photo/2025/08/28/18/42/hamberger-10037363_1280.jpg',
-    rating: '1',
-  },
-  {
-    id: 6,
-    name: 'sushi',
-    image:
-      'https://cdn.pixabay.com/photo/2025/08/28/18/42/hamberger-10037363_1280.jpg',
-    rating: '0',
-  },
-  {
-    id: 7,
-    name: 'tteokbokki',
-    image:
-      'https://cdn.pixabay.com/photo/2025/08/28/18/42/hamberger-10037363_1280.jpg',
-    rating: '0',
-  },
-  {
-    id: 8,
-    name: 'bibimbap',
-    image:
-      'https://cdn.pixabay.com/photo/2025/08/28/18/42/hamberger-10037363_1280.jpg',
-    rating: '0',
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {foodLike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  picture: PropTypes.string.isRequired,
-};
 
 export default App;
