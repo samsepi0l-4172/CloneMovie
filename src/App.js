2e90dc9feaa9cd2450b8c7f7ae9881f4ffb0151b
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2025-11-22 00:17:26 +0900

    6.2.0

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
   };
   getMovies = async () => {
     const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    console.log(movies.data.data.movies);
   };
   componentDidMount() {
     this.getMovies();
